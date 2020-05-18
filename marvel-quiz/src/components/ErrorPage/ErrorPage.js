b624962a0db475b1e12fb58fda4dd74aefa0d0ee
Boumarouane <i.boumarouane@gmail>
2020-05-18 07:12:35 +0200

    commencement du projet et react router

--- marvel-quiz/src/components/ErrorPage/ErrorPage.js
+++ marvel-quiz/src/components/ErrorPage/ErrorPage.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import batman from "../../images/batman.png";
+
+const centerTitle = {
+    textAlign: 'center',
+    marginTop: '50px'
+}
 
 const ErrorPage = () => {
     return (
         <div className='quiz-bg'>
             <div className="container">
-                ErrorPage
+                <h2 style={centerTitle}>Cette page n'existe pas !</h2>
+                <img src={batman} alt="error page"/>
             </div>
         </div>
     );
