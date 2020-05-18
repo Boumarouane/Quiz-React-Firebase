7d25c23485c179507aab64cd0021084840611ba5
Boumarouane <i.boumarouane@gmail>
2020-05-18 07:15:07 +0200

    csss in js

--- marvel-quiz/src/components/ErrorPage/ErrorPage.js
+++ marvel-quiz/src/components/ErrorPage/ErrorPage.js
@@ -6,6 +6,11 @@ const centerTitle = {
     marginTop: '50px'
 }
 
+const centerImg = {
+    display: "block",
+    margin: "40px auto"
+}
+
 const ErrorPage = () => {
     return (
         <div className='quiz-bg'>
