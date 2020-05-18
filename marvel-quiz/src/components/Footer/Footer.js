c6614ccbbe0d4d17188623ea524c76315e9cb073
Boumarouane <i.boumarouane@gmail>
2020-05-18 06:51:39 +0200

    maj

---
+++ marvel-quiz/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className='footer-container'>
+                <p>Projet réalisé par DonkayGeek - 2020</p>
+                <p>Les icones sont prises sur iconFinder.com</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
