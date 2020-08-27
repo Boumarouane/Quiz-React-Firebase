9eda973f966b4427c45447f639d71e6e90f948ff
Boumarouane <i.boumarouane@gmail.com>
2020-08-27 22:01:43 +0200

    ajout de react router

--- naruto-quiz/src/App.js
+++ naruto-quiz/src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/Header/Header';
 import Landing from './components/Landing/Landing';
 import Footer from './components/Footer/Footer';
+import Welcome from './components/Welcome/Welcome'
+import Login from './components/Login/Login'
+import Signup from './components/Signup/Signup'
+import ErrorPage from './components/ErrorPage/ErrorPage'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+
       <Header/>
-      <Landing/>
+
+      <Route  exact path="/" component={Landing}/>
+      <Route  path="/welcome" component={Welcome}/>
+      <Route  path="/login" component={Login}/>
+      <Route  path="/signup" component={Signup}/>
+      <Route  component={ErrorPage}/>
+
       <Footer/>
-    </div>
+
+    </Router>
   );
 }
 
--- naruto-quiz/src/components/Landing/Landing.js
+++ naruto-quiz/src/components/Landing/Landing.js
@@ -1,4 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+
 
 const Landing = () => {
 
@@ -7,7 +9,7 @@ const Landing = () => {
     // Faire une reference à la balise main
     const refWolverine = useRef(null);
 
-    // useEffect permetra monter un composant qui va acceder à la class de la reference pour pouvoir la changer.
+    // useEffect permetra de monter un composant qui va acceder à la class de la reference pour pouvoir la changer.
     useEffect(() => {
         refWolverine.current.classList.add('startingImg');
         setTimeout(() => {
@@ -15,14 +17,14 @@ const Landing = () => {
             setBtn(true)
         }, 3000)
     }, []);
-
+    // Ajout des class contenant les images en onMouseOver
     const setLeftImg = () => {
         refWolverine.current.classList.add('leftImg');
     }
     const setRightImg = () => {
         refWolverine.current.classList.add('rightImg');
     }
-
+    // Condition pour savoir si l'une des deux images existe, et si oui l'autre sera suprimé tous en onMouseOut
     const clearImg = () => {
         if (refWolverine.current.classList.contains('leftImg')){
             refWolverine.current.classList.remove('rightImg')
@@ -34,10 +36,10 @@ const Landing = () => {
     const displayBtn = btn && (
         <>
             <div onMouseOver={setLeftImg} onMouseOut={clearImg} className="leftBox">
-                <button className="btn-welcome">Inscription</button>
+                <Link to="/signup" className="btn-welcome">Inscription</Link>
             </div>
             <div onMouseOver={setRightImg} onMouseOut={clearImg} className="rightBox">
-                <button className="btn-welcome">Connexion</button>
+                <Link to="/login" className="btn-welcome">Connexion</Link>
             </div>
         </>
     )
