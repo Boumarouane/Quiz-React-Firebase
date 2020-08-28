0471797bf69fc9b9cd41364d56d0274e509d9666
Boumarouane <i.boumarouane@gmail.com>
2020-08-28 21:09:07 +0200

    ajout d'une image dans la page error

--- naruto-quiz/src/components/ErrorPage/ErrorPage.js
+++ naruto-quiz/src/components/ErrorPage/ErrorPage.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Batman from '../../images/batman.png'
+
+const centerh2 = {
+    textAlign: 'center',
+    marginTop: '50px'
+}
+
+const imageCenter = {
+    display: 'block',
+    width: '100%',
+    height: '100%'
+}
 
 const ErrorPage = () => {
     return (
         <div className="quiz-bg">
             <div className="container">
-                ErrorPage
+                <h2 style={centerh2}>Oups tu t'es gouré gars</h2>
+                <img style = {imageCenter}src={ Batman } alt="logo de batman"></img>
             </div>
         </div>
     );
