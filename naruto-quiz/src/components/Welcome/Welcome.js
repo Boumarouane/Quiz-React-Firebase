2d742a7e46c4da7c4bcb842583254d1b35cbbce0
Boumarouane <i.boumarouane@gmail.com>
2020-09-02 11:57:42 +0200

    gestion de la session utilisateur

--- naruto-quiz/src/components/Welcome/Welcome.js
+++ naruto-quiz/src/components/Welcome/Welcome.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import Logout from '../Logout/Logout';
-import Quiz from '../Quiz/Quiz'
+import Quiz from '../Quiz/Quiz';
+import { FirebaseContext } from '../firebase/encapsule';
 
-const Welcome = () => {
-    return (
+
+const Welcome = props => {
+
+    const firebase = useContext(FirebaseContext);
+
+    // Contenant l'information concernant l'authentification de la session
+    const [userSession, setUserSession] = useState(null);
+
+    // Permet de detecter si l'utilisateur est authentifier puis on doit démonter l'evenement lorqu'il est utilisé via la fonction callback
+    useEffect(() => {
+        let listener = firebase.auth.onAuthStateChanged(user => {
+            user ? setUserSession(user) : props.history.push('/')
+        })
+        return () => {
+            listener()
+        };
+    }, [props.history]);
+
+    // On vérifie si le state est rempli si non on met le loader et si oui on met le quiz
+    return userSession === null ? (
+        <>
+            <div className="loader"></div>
+            <p className="loaderText">Loader ...</p>
+        </>
+    ) : (
         <div className="quiz-bg">
             <div className="container">
                 <Logout/>
                 <Quiz/>
             </div>
         </div>
-    );
+    )
 };
 
 export default Welcome;
