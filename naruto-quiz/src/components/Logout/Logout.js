15dbea229589d27aca5b6d7aab259e82288a7178
Boumarouane <i.boumarouane@gmail.com>
2020-09-02 09:41:56 +0200

    ajout de la déconnexion

---
+++ naruto-quiz/src/components/Logout/Logout.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect, useContext} from 'react';
+import { FirebaseContext } from '../firebase/encapsule';
+
+
+const Logout = () => {
+
+    const [checked, setCheked] = useState(false);
+
+    const firebase = useContext(FirebaseContext);
+
+    // Si le checked est true alors on la personne est déco (firebase sais comment gérer ca)
+    useEffect(() => {
+        if(checked){
+            console.log('Je suis putain de déconnecter de la vie');
+            firebase.signOutUser()
+        }
+    }, [checked, firebase])
+
+    // On change le state checked en true lorsque la personne clique sur l'input
+    const handleChange = e => {
+        setCheked(e.target.checked)
+    }
+
+    return (
+        <div className="logoutContainer">
+            <label className="switch">
+                <input  onChange={handleChange} type="checkbox" checked={checked}/>
+                <span className="slider round"></span>
+            </label>
+        </div>
+    );
+};
+
+export default Logout;
--- naruto-quiz/src/components/Welcome/Welcome.js
+++ naruto-quiz/src/components/Welcome/Welcome.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Logout from '../Logout/Logout';
+import Quiz from '../Quiz/Quiz'
 
 const Welcome = () => {
     return (
         <div className="quiz-bg">
             <div className="container">
-                Welcome
+                <Logout/>
+                <Quiz/>
             </div>
         </div>
     );
