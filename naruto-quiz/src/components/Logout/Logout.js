db6ff0ca3fec17cb4cc3d3d6c64753eaed628dd0
Boumarouane <i.boumarouane@gmail.com>
2020-11-13 15:16:47 +0100

    ajout du tooltip

--- naruto-quiz/src/components/Logout/Logout.js
+++ naruto-quiz/src/components/Logout/Logout.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, useContext} from 'react';
 import { FirebaseContext } from '../firebase/encapsule';
+import ReactToolTip from 'react-tooltip';
 
 
 const Logout = () => {
@@ -25,8 +26,12 @@ const Logout = () => {
         <div className="logoutContainer">
             <label className="switch">
                 <input  onChange={handleChange} type="checkbox" checked={checked}/>
-                <span className="slider round"></span>
+                <span className="slider round" data-tip="Déconnexion"></span>
             </label>
+            <ReactToolTip
+                place="left"
+                effect="solid"
+            />
         </div>
     );
 };
